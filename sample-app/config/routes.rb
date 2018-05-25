fd15865f75e9520538a79148c9a8aad021feeb42
kamal-webonise <kamal.singh@weboniselab.com>
2018-05-25 19:14:00 +0530

    Sign Up

--- sample-app/config/routes.rb
+++ sample-app/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/help' ,to: 'static_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  post '/signup', to: 'users#create'
+  resources :users
 end
