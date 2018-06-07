29228462af83b329f3bcad389de48cfb686686e6
kamal-webonise <kamal.singh@weboniselab.com>
2018-06-07 19:34:02 +0530

    activation

--- sample-app/app/controllers/users_controller.rb
+++ sample-app/app/controllers/users_controller.rb
@@ -10,18 +10,19 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
   end
 
   def index
-  	@users = User.paginate(page: params[:page]) 
+  	@users = User.where(activated: FILL_IN).paginate(page: params[:page])
   end
 
   def create
   	@user = User.new(user_params)
   	if @user.save
-      log_in @user
-  		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your mail to activate your account."
+      redirect_to root_url
   	else
   		render 'new'
   	end		
--- sample-app/app/mailers/application_mailer.rb
+++ sample-app/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'kamalmehra79531@gmail.com'
   layout 'mailer'
 end
